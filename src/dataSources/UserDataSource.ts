359ec7672a1e5651e41229bee08937787813df3a
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2022-09-14 10:35:58 -0700

    add env variables to typeorm datasource connection

--- src/dataSources/UserDataSource.ts
+++ src/dataSources/UserDataSource.ts
@@ -1,15 +1,16 @@
+require('dotenv').config()
 import { DataSource } from 'typeorm'
-import { User } from '../entity/User';
+import { UserEntity } from '../entity/UserEntity';
 
 const UserDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "mrdontacos",
-    password: "",
-    database: "poke_g_api",
+    host: process.env.MYSQL_USERNAME,
+    port: parseInt(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     connectTimeout: 25000,
-    entities: [User]
+    entities: [UserEntity]
 });
 
 export default UserDataSource;
